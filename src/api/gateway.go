b9b7d0a43a510399db50d7fa2337f174f4b64332
Mahdi Arezoumandi <center328.a@gmail.com>
2021-10-19 02:15:11 +0330

    fix undefined funcs bug

--- src/api/gateway.go
+++ src/api/gateway.go
@@ -4,9 +4,14 @@ import (
 	"bytes"
 	"encoding/json"
 	"errors"
+	"github.com/aws/aws-sdk-go/aws"
+	awsSqs "github.com/aws/aws-sdk-go/service/sqs"
+	"github.com/center328/task-lambda-sqs-dynamodb/src/lib/db"
+	"github.com/google/uuid"
 	"log"
 	"os"
-	"github.com/center328/task-lambda-sqs-dynamodb/src/lib/db"
+	"strconv"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -92,6 +97,42 @@ func ValidateInputs(request Request) (RequestBody, error) {
 	// Everything looks fine, return created NewDevice in Go struct.
 	return NewBody, nil
 } // End of ValidateInputs function.
+
+func createRequests(size int, body string, requestID string) []db.Record {
+	var records []db.Record
+
+	var temp = db.Record{
+		ID: "",
+		ProcessStatus: false,
+		Data: body,
+		RequestID: requestID,
+		RequestDate: time.Now().String(),
+		ProcessDate: "",
+	}
+
+	for i := 0; i < size; i++ {
+		temp.ID = uuid.NewString()
+		records = append(records, temp)
+	}
+
+	return records
+}
+
+func createMessagesToEnqueue(msgs []db.Record)  []*awsSqs.SendMessageBatchRequestEntry {
+	var msgBatch []*awsSqs.SendMessageBatchRequestEntry
+	for i := 0; i < len(msgs); i++ {
+		data, _ := json.Marshal(msgs[i])
+		message := &awsSqs.SendMessageBatchRequestEntry{
+			Id:                     aws.String(`uniqueID_` + strconv.Itoa(i)),
+			MessageBody:            aws.String(string(data)),
+			MessageDeduplicationId: aws.String(`dupID_` + strconv.Itoa(i)),
+			MessageGroupId:         aws.String("task1Queue"),
+		}
+		msgBatch = append(msgBatch, message)
+	}
+
+	return msgBatch
+}
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(request Request) (Response, error) {
